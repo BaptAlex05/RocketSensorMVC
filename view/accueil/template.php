9028394a68e3975acfd52b492ea66c40f17ae5c9
Baptiste <48565076+BaptAlex05@users.noreply.github.com>
2020-05-26 17:25:26 +0200

    Update

---
+++ view/accueil/template.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="uft-8" />
+		<link rel="icon" href="../public/images/logo_rocket-sensor.png" />
+		<title>Rocket Sensor - <?= $title ?></title>
+		<link rel="stylesheet" href="../../public/css/style.css" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+	</head>
+
+	<body>
+		<?= $content ?>
+
+		<div id="footer">
+			<footer>
+				<div id="bloc_footer">
+
+					<nav id="menu_footer">
+						<ul>
+							<li><a href="cgu.php">Conditions générales d'utilisation</a></li>
+							<li><a href="mentions_legales.php">Mentions légales</a></li>
+					</nav>
+
+					<div id="copyright">
+						<p>© 2020 Rocket Sensor - Tous droits réservés.<br/>
+							Site réalisé avec ❤️ par Bamboo</p>
+					</div>	
+
+					<div id="logo_infinite-measures">
+						<img src="../../public/images/infinite-measures.png" alt="logo_infinite-measures">
+					</div>
+
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
